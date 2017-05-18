c83c922b6f202af46b12732953046b23f9a8293f
Alexia Hurley <AlexiaAmiHurley@gmail.com>
2017-05-18 16:22:35 -0400

    Layout #1

--- application/modules/auth/views/permission_roles.php
+++ application/modules/auth/views/permission_roles.php
@@ -1,108 +1,136 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-
-<div style="height:400px; width: 1000px; border: solid 1px">
-	<!--Left-->
-	<div style="float:left; height: 100%; width:25%; border-right: solid 5px; clear:none">
-		
-		<div style="width:100%; border-bottom:solid 1px">
-			<strong>Roles</strong>
+<div class="container-fluid" style="width:95%;height:500px;">
+	<div class="row" style="height:100%">
+	
+	
+		<div class="col-md-2" style="height:100%;padding:0px">
+			<div class="panel panel-default" style="height:100%;">
+				<div class="panel-heading"> 
+					<div class="container-fluid">
+						<div class="row">
+							<div class="col-md-6" style="padding-left:0px;padding-right:0px"><strong>Roles:</strong></div>
+							<div class="col-md-6" style="padding-left:0px;padding-right:0px">
+								<input type="text" class="input-xs" style="width:100%;" placeholder="Search">
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="panel-body">
+					<ul style="padding-left:0px;list-style:none;">
+						<li><i class="glyphicon glyphicon-menu-right"></i> Advising</li>
+						<li><i class="glyphicon glyphicon-menu-up"></i> Enrollment</li>
+						<li><i class="glyphicon glyphicon-menu-up"></i> Athletics</li>	
+					</ul>
+				</div>
+				<div class="panel-footer" style="position:absolute;bottom:0; width:100%;text-align:center">
+					<button type="button" class="btn btn-default btn-sm" >
+						<i class="glyphicon glyphicon-plus"></i> New Role 
+					</button>
+				</div>
+			</div>
 		</div>
 		
-		<div>
-			<ul style="list-style:none; padding-left: 10px; margin-top:5px;">
-				<li style="padding-top:2px; padding-bottom:5px">Instructor</li>
-				<li style="padding-top:2px; padding-bottom:5px">College Advising</li>
-				<li style="padding-top:2px; padding-bottom:5px">Information Technology</li>
-			</ul>
+		<div class="col-md-10 container-fluid" style="height:100%;padding:0px">
+			<div class="row" style="height:100%">
+				<div class="col-md-11 col-md-offset-1 container-fluid" style="height:100%;">
+							
+					<div class="row" style="height:55%;">
+						<div class="col-md-12" style="height:100%;">
+							<div class="panel panel-default" style="height:100%;">
+								<div class="panel-heading">
+									<strong>Permissions (Advising):</strong>
+									<button type="button" class="btn btn-default btn-sm">
+										<i class="glyphicon glyphicon-remove"></i> Delete Role 
+									</button>
+									<span style="float:right">
+										<input type="text" placeholder="Search">
+									</span>
+								</div>
+								<div class="panel-body" style="height:100%">
+									<table class="table-striped" style="width:100%;max-height:100%;">
+										<tr>
+											<th style="width:10%">Remove</th>
+											<th style="width:35%">Category</th>
+											<th style="width:55%">Action</th>
+											
+										</tr>
+										<tr>
+											<td style="width:10%"><span class="glyphicon glyphicon-remove"></span></td>
+											<td style="width:35%">All Users</td>
+											<td style="width:55%">Can Use Application</td>
+										</tr>
+									</table>
+								</div>
+							
+							</div>
+						</div>
+					</div>
+					<div class="row" style="height:5%"></div>
+					<div class="row" style="height:40%;">
+						<div class="col-md-6 container-fluid" style="height:100%">
+							<div class="row" style="height:100%">
+								<div class="col-md-11 pull-md-1" style="height:100%;">
+									<div class="panel panel-default" style="height:100%;position:relative">
+										<div class="panel-heading"> 
+											<div class="container-fluid">
+												<div class="row">
+													<div class="col-md-6" style="padding-left:0px"><strong>Category:</strong></div>
+													<div class="col-md-6" style="padding-right:0px">
+														<input type="text" class="input-xs" style="width:100%;" placeholder="Search">
+													</div>
+												</div>
+											</div>
+										</div>
+										<div class="panel-body"> 
+											<ul style="padding-left:0px;list-style:none;">
+												<li><i class="glyphicon glyphicon-menu-up"></i> Calender</li>
+												<li><i class="glyphicon glyphicon-menu-up"></i> Records</li>
+												<li><i class="glyphicon glyphicon-menu-right"></i> Courses</li>
+											</ul>
+										</div>
+										<div class="panel-footer" style="position:absolute;bottom:0; width:100%;text-align:center">
+											<button type="button" class="btn btn-default btn-sm" >
+												<i class="glyphicon glyphicon-plus"></i> New Category 
+											</button>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-6 container-fluid" style="height:100%">
+							<div class="row" style="height:100%">
+								<div class="col-md-11 col-md-offset-1" style="height:100%;">
+									<div class="panel panel-default" style="height:100%;position:relative">
+										<div class="panel-heading"> 
+											<div class="container-fluid">
+												<div class="row">
+													<div class="col-md-6" style="padding-left:0px"><strong>Action:</strong></div>
+													<div class="col-md-6" style="padding-right:0px">
+														<input type="text" class="input-xs" style="width:100%;" placeholder="Search">
+													</div>
+												</div>
+											</div>
+										</div>
+										<div class="panel-body"> 
+											<ul style="padding-left:0px;list-style:none;">
+												<li><i class="glyphicon glyphicon-plus"></i> View Schedule</li>
+												<li><i class="glyphicon glyphicon-plus"></i> Edit Schedule</li>
+												<li><i class="glyphicon glyphicon-plus"></i> Override Courses</li>
+											</ul>
+										</div>
+										<div class="panel-footer" style="position:absolute;bottom:0; width:100%;text-align:center">
+											<button type="button" class="btn btn-default btn-sm" >
+												<i class="glyphicon glyphicon-plus"></i> New Action 
+											</button>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					
+					
+				</div>
+			</div>
 		</div>
 	</div>
-	
-	<!--Right-->
-	<div style="width:75%; height: 100%; float:left; clear:none">
-		<!--Top-->
-		<div style="border-bottom:solid 5px; height: 66%; width:100%">
-			
-			<!--Permission Code-->
-			<div style="width:25%; height:100%; border-right: 1px solid; float:left; clear:none">
-				<div style="width:100%; border-bottom:solid 1px">
-					<strong>Category</strong>
-				</div>
-				
-				<div>
-					<ul style="list-style:none; padding-left: 10px; margin-top:5px;">
-						<li style="padding-top:2px; padding-bottom:5px">All Users</li>
-						<li style="padding-top:2px; padding-bottom:5px">Information Technology</li>
-						<li style="padding-top:2px; padding-bottom:5px">Advising and Counseling</li>
-					</ul>
-				</div>
-			</div>
-			
-			<!--Permission Description-->
-			<div style="width:60%; height:100%; border-right: 1px solid; float:left; clear:none">
-				<div style="width:100%; border-bottom:solid 1px">
-					<strong>Action</strong>
-				</div>		
-
-				<div>
-					<ul style="list-style:none; padding-left: 10px; margin-top:5px;">
-						<li style="padding-top:2px; padding-bottom:5px">Can Use Application</li>
-						<li style="padding-top:2px; padding-bottom:5px">Remote Desktop</li>
-						<li style="padding-top:2px; padding-bottom:5px">View Student Records</li>
-					</ul>
-				</div>				
-			</div>
-			
-			<!--"Remove Row" Link-->
-			<div style="width:15%; height:100%; float:left; clear:none">
-				<div style="width:100%; border-bottom:solid 1px">
-					<strong>Remove</strong>
-				</div>		
-
-				<div>
-					<ul style="list-style:none; padding-left: 10px; margin-top:5px;">
-						<li style="padding-top:2px; padding-bottom:5px">Remove</li>
-						<li style="padding-top:2px; padding-bottom:5px">Remove</li>
-						<li style="padding-top:2px; padding-bottom:5px">Remove</li>
-					</ul>
-				</div>						
-			</div>
-		
-		</div>
-		
-		<!--Bottom-->
-		<div style="height:34%; width:100%">
-			<!---Category-->
-			<div style="width:50%; height:100%; border-right:5px solid; float:left; clear:none">
-				<div style="width:100%; border-bottom:solid 1px; padding-bottom:5px;">
-					<strong>Categories:</strong>
-					<input type="text" placeholder="Search" style="margin-left:10px">
-				</div>
-				
-				<div>
-					<ul style="list-style:none; padding-left: 10px; margin-top:5px;">
-						<li style="padding-top:2px; padding-bottom:5px">General</li>
-						<li style="padding-top:2px; padding-bottom:5px">Student</li>
-						<li style="padding-top:2px; padding-bottom:5px">Finance</li>
-					</ul>
-				</div>		
-			</div>
-			
-			<!---Action-->
-			<div style="width:50%; height:100%; float:left; clear:none">
-				<div style="width:100%; border-bottom:solid 1px; padding-bottom:5px;">
-					<strong>Actions:</strong>
-					<input type="text" placeholder="Search" style="margin-left:10px">
-				</div>
-				
-				<div>
-					<ul style="list-style:none; padding-left: 10px; margin-top:5px;">
-						<li style="padding-top:2px; padding-bottom:5px">View Student Courses</li>
-						<li style="padding-top:2px; padding-bottom:5px">Edit Student Courses</li>
-						<li style="padding-top:2px; padding-bottom:5px">View Notes</li>
-					</ul>
-				</div>	
-			</div>
-		</div>
-	</div>
-	
 </div>
